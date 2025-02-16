1186917df931e0e64f4f07b4bc794d1189ca340d
gri11 <kanawatgrill2002@gmail.com>
2025-02-16 20:36:15 +0700

    add evaluate cli

--- comet/cli/evaluate.py
+++ comet/cli/evaluate.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+
 import pandas as pd
 import wandb
 from sklearn.metrics import classification_report
+from pprint import pprint
 
 
 def evaluate(model, file_path):
@@ -25,7 +28,22 @@ def evaluate(model, file_path):
         
         metrics[f'{file_path}_recall@bottom20'] = report['True']['recall']
         metrics[f'{file_path}_report'] = report
-        
-    wandb.log(metrics)
+    
+    if wandb.run is not None:
+        wandb.log(metrics)
+    else:
+        pprint(metrics)
     
     return metrics
+
+if __name__ == '__main__':
+    import argparse
+    from comet import load_from_checkpoint
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_path', type=str, required=True)
+    parser.add_argument('--file_path', type=str, required=True)
+    args = parser.parse_args()
+
+    model = load_from_checkpoint(args.model_path)
+    evaluate(model, args.file_path)
